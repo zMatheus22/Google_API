ea90091a63b1ec0b0e97e96f3c11f0cb9396cc0c
zMatheus22 <matheusv.strada4@gmail.com>
2023-05-09 00:06:55 -0300

    edição nas variáveis de ambiente (dotenv) e alteração na chamada a credencial

--- backend/src/model/exitSheetModel.js
+++ backend/src/model/exitSheetModel.js
@@ -5,8 +5,20 @@ const nameRange = process.env.NAMERANGE;
 
 // Autenticação
 const auth = new google.auth.GoogleAuth({
-  keyFile: "credentials.json",
-  scopes: "https://www.googleapis.com/auth/spreadsheets",
+  credentials: {
+    type: process.env.CREDENTIALS_TYPE,
+    project_id: process.env.CREDENTIALS_PROJECT_ID,
+    private_key_id: process.env.CREDENTIALS_PRIVATE_KEY_ID,
+    private_key: process.env.CREDENTIALS_PRIVATE_KEY,
+    client_email: process.env.CREDENTIALS_CLIENT_EMAIL,
+    client_id: process.env.CREDENTIALS_CLIENT_ID,
+    auth_uri: process.env.CREDENTIALS_AUTH_URI,
+    token_uri: process.env.CREDENTIALS_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CREDENTIALS_CLIENT_X509_CERT_URL,
+  },
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
 // Criando o cliente para utilizar a planilha
@@ -50,7 +62,7 @@ const createRows = async (row) => {
     range: `${nameRange}!A:E`,
     valueInputOption: "USER_ENTERED",
     resource: {
-      values: [[ id, data, model, quantity, plate, mechanic]],
+      values: [[id, data, model, quantity, plate, mechanic]],
     },
   });
 };
